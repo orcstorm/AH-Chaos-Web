73d1ffc4c54381e607fe44bfcd1caad2a4b67bc1
Donald <don.mennerich@nyu.edu>
2017-07-24 17:07:46 -0400

    adding initial form

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,42 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.i18n.MessagesApi
+import play.api.i18n.I18nSupport
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+object Protocol {
+  case class PullData(campaign: String, difficulty: String)
+}
+
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(messages: MessagesApi) extends AbstractController(cc) with I18nSupport {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  import Protocol._
+  
+  val pullForm = Form(
+    mapping(
+      "campaign" -> text,
+      "difficulty" -> text)
+    (PullData.apply)(PullData.unapply))
+
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index(pullForm))
+  }
+
+  def pull() = Action { implicit request =>
+      pullForm.bindFromRequest.fold(
+       formWithErrors => {
+         Ok("nope")
+       },
+      
+      data => {
+        Ok(s"${data.campaign} ${data.difficulty}")
+      }  
+      )
   }
 }
+
+
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -21,7 +21,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Arkham Horror Chaos Bag Sim")
     }
 
     "render the index page from the application" in {
@@ -30,7 +30,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Arkham Horror Chaos Bag Sim")
     }
 
     "render the index page from the router" in {
@@ -39,7 +39,8 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Arkham Horror Chaos Bag Sim")
     }
+    
   }
 }
