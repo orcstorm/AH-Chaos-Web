7c9d058619942bf9038c732caf03d49218723793
Donald Mennerich <don.mennerich@nyu.edu>
2017-07-26 14:13:56 -0400

    updating formatting

--- app/helpers/ArkhamChaos.scala
+++ app/helpers/ArkhamChaos.scala
@@ -6,7 +6,7 @@ object ArkhamChaos {
   
   
   val zealotBag: Map[String, Vector[Int]] = Map(
-	  "easy" -> Vector(1,1, 0,0,-1, -1, -1, -2, -2, -100, -100, -200, -300, -400, -500),
+    "easy" -> Vector(1,1, 0,0,-1, -1, -1, -2, -2, -100, -100, -200, -300, -400, -500),
     "standard" -> Vector(1, 0,0,-1, -1, -1, -2, -2, -3, -4, -100, -100, -200, -300, -400, -500),
     "hard" -> Vector(0,0,0,-1,-1,-2, -2, -3, -3, -4, -5, -100, -100, -200, -300, -400, -500),
     "expert" -> Vector(0,-1, -1, -2, -2, -3, -3, -4, -4, -5, -6, -100, -100, -200, -300, -400, -500)
@@ -22,14 +22,14 @@ object ArkhamChaos {
   val bags: Map[String, Map[String, Vector[Int]]] = Map("zealot" -> zealotBag, "dunwich" -> dunwichBag)
 
   def pullToken(campaign: String, difficulty: String): String = {
-  	val bag = bags(campaign)(difficulty)
-  	val r = scala.util.Random
-  	val token = bag(r.nextInt(bag.size))
+    val bag = bags(campaign)(difficulty)
+    val r = scala.util.Random
+    val token = bag(r.nextInt(bag.size))
 
-  	specialTokens.contains(token) match {
-  	  case true => specialTokens(token)
-  	  case false => token.toString
-  	}
+    specialTokens.contains(token) match {
+      case true => specialTokens(token)
+      case false => token.toString
+    }
   }
 
 }
